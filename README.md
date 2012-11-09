41ab8b35fe43aa3c4a33788064215b40840ab100
davedes <dave.des@gmail.com>
2012-11-09 14:26:12 -0500

    replicating github bug

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+## About
+
 *lwjgl-basics* is a minimal shader-based library for 2D sprite games. It provides essential utilities for textures, shaders, and sprite rendering.
 
 For a large game project, a platform like [LibGDX](http://libgdx.badlogicgames.com/) may be more suitable.
@@ -7,7 +9,7 @@ The [source code](https://github.com/mattdesl/lwjgl-basics) is hosted on GitHub.
 ### Snippets
 
 The wiki will also host various code snippets and tutorials for LWJGL and OpenGL:  
-[https://github.com/mattdesl/lwjgl-basics/wiki](https://github.com/mattdesl/lwjgl-basics/wiki)
+https://github.com/mattdesl/lwjgl-basics/wiki
 
 ### Credits
 
--- src/mdesl/graphics/Color.java
+++ src/mdesl/graphics/Color.java
@@ -1,191 +1,168 @@
 package mdesl.graphics;
 
-
-/**
- * A minimal Color utility, which holds four float values representing RGBA.
+/** A minimal Color utility, which holds four float values representing RGBA.
  * 
- * @author davedes
- */
+ * @author davedes */
 public class Color {
 
 	/** The fixed color transparent */
-    public static final Color TRANSPARENT = new Color(0.0f,0.0f,0.0f,0.0f);
+	public static final Color TRANSPARENT = new Color(0.0f, 0.0f, 0.0f, 0.0f);
 	/** The fixed colour white */
-	public static final Color WHITE = new Color(1.0f,1.0f,1.0f,1.0f);
+	public static final Color WHITE = new Color(1.0f, 1.0f, 1.0f, 1.0f);
 	/** The fixed colour yellow */
-	public static final Color YELLOW = new Color(1.0f,1.0f,0,1.0f);
+	public static final Color YELLOW = new Color(1.0f, 1.0f, 0, 1.0f);
 	/** The fixed colour red */
-	public static final Color RED = new Color(1.0f,0,0,1.0f);
+	public static final Color RED = new Color(1.0f, 0, 0, 1.0f);
 	/** The fixed colour blue */
-	public static final Color BLUE = new Color(0,0,1.0f,1.0f);
+	public static final Color BLUE = new Color(0, 0, 1.0f, 1.0f);
 	/** The fixed colour green */
-	public static final Color GREEN = new Color(0,1.0f,0,1.0f);
+	public static final Color GREEN = new Color(0, 1.0f, 0, 1.0f);
 	/** The fixed colour black */
-	public static final Color BLACK = new Color(0,0,0,1.0f);
+	public static final Color BLACK = new Color(0, 0, 0, 1.0f);
 	/** The fixed colour gray */
-	public static final Color GRAY = new Color(0.5f,0.5f,0.5f,1.0f);
+	public static final Color GRAY = new Color(0.5f, 0.5f, 0.5f, 1.0f);
 	/** The fixed colour cyan */
-	public static final Color CYAN = new Color(0,1.0f,1.0f,1.0f);
+	public static final Color CYAN = new Color(0, 1.0f, 1.0f, 1.0f);
 	/** The fixed colour dark gray */
-	public static final Color DARK_GRAY = new Color(0.3f,0.3f,0.3f,1.0f);
+	public static final Color DARK_GRAY = new Color(0.3f, 0.3f, 0.3f, 1.0f);
 	/** The fixed colour light gray */
-	public static final Color LIGHT_GRAY = new Color(0.7f,0.7f,0.7f,1.0f);
+	public static final Color LIGHT_GRAY = new Color(0.7f, 0.7f, 0.7f, 1.0f);
 	/** The fixed colour dark pink */
-    public final static Color PINK      = new Color(255, 175, 175, 255);
+	public final static Color PINK = new Color(255, 175, 175, 255);
 	/** The fixed colour dark orange */
-    public final static Color ORANGE 	= new Color(255, 200, 0, 255);
+	public final static Color ORANGE = new Color(255, 200, 0, 255);
 	/** The fixed colour dark magenta */
-    public final static Color MAGENTA	= new Color(255, 0, 255, 255);
-    
-	public float r, g, b, a;
+	public final static Color MAGENTA = new Color(255, 0, 255, 255);
 	
-	/**
-	 * Create a 4 component colour
+	/** The red component [0.0 - 1.0]. */
+	public float r;
+	/** The green component [0.0 - 1.0]. */
+	public float g;
+	/** The blue component [0.0 - 1.0]. */
+	public float b;
+	/** The alpha component [0.0 - 1.0]. */
+	public float a;
+
+	/** Create a 4 component colour
 	 * 
 	 * @param r The red component of the colour (0.0 -> 1.0)
 	 * @param g The green component of the colour (0.0 -> 1.0)
 	 * @param b The blue component of the colour (0.0 -> 1.0)
-	 * @param a The alpha component of the colour (0.0 -> 1.0)
-	 */
+	 * @param a The alpha component of the colour (0.0 -> 1.0) */
 	public Color(float r, float g, float b, float a) {
 		this.r = r;
 		this.g = g;
 		this.b = b;
 		this.a = a;
 	}
-	
-	/**
-	 * Create a 3 component colour; alpha is passed as 1.0 (255).
+
+	/** Create a 3 component colour; alpha is passed as 1.0 (255).
 	 * 
 	 * @param r The red component of the colour (0.0 -> 1.0)
 	 * @param g The green component of the colour (0.0 -> 1.0)
-	 * @param b The blue component of the colour (0.0 -> 1.0)
-	 */
+	 * @param b The blue component of the colour (0.0 -> 1.0) */
 	public Color(float r, float g, float b) {
 		this(r, g, b, 1f);
 	}
 
-	/**
-	 * Create a 4 component colour
+	/** Create a 4 component colour
 	 * 
 	 * @param r The red component of the colour (0 -> 255)
 	 * @param g The green component of the colour (0 -> 255)
 	 * @param b The blue component of the colour (0 -> 255)
-	 * @param a The alpha component of the colour (0 -> 255)
-	 */
+	 * @param a The alpha component of the colour (0 -> 255) */
 	public Color(int r, int g, int b, int a) {
-		this(r/255f, g/255f, b/255f, a/255f);
+		this(r / 255f, g / 255f, b / 255f, a / 255f);
 	}
 
-	/**
-	 * Create a 3 component colour; alpha is passed as 255 (1.0).
+	/** Create a 3 component colour; alpha is passed as 255 (1.0).
 	 * 
 	 * @param r The red component of the colour (0 -> 255)
 	 * @param g The green component of the colour (0 -> 255)
-	 * @param b The blue component of the colour (0 -> 255)
-	 */
+	 * @param b The blue component of the colour (0 -> 255) */
 	public Color(int r, int g, int b) {
 		this(r, g, b, 255);
 	}
 
-	/**
-	 * Copy constructor
+	/** Copy constructor
 	 * 
-	 * @param color The color to copy into the new instance
-	 */
+	 * @param color The color to copy into the new instance */
 	public Color(Color color) {
 		this(color.r, color.g, color.b, color.a);
 	}
-	
-	/**
-	 * Create a colour from an integer packed 0xAARRGGBB. If AA 
-	 * is specified as zero then it will be interpreted as unspecified
-	 * and hence a value of 255 will be recorded.
+
+	/** Create a colour from an integer packed 0xAARRGGBB. If AA is specified as
+	 * zero then it will be interpreted as unspecified and hence a value of 255
+	 * will be recorded.
 	 * 
-	 * @param value The value to interpret for the colour
-	 */
+	 * @param value The value to interpret for the colour */
 	public Color(int value) {
 		int r = (value & 0x00FF0000) >> 16;
 		int g = (value & 0x0000FF00) >> 8;
-		int b =	(value & 0x000000FF);
+		int b = (value & 0x000000FF);
 		int a = (value & 0xFF000000) >> 24;
-		if (a < 0) 
+		if (a < 0)
 			a += 256;
 		if (a == 0)
-			a = 255;		
+			a = 255;
 		this.r = r / 255.0f;
 		this.g = g / 255.0f;
 		this.b = b / 255.0f;
 		this.a = a / 255.0f;
 	}
-	
-	/**
-	 * Decode a number in a string and process it as a colour.
+
+	/** Decode a number in a string and process it as a colour.
 	 * 
 	 * @param nm The number string to decode
 	 * @return The color created from the number read
-	 * @throws NumberFormatException if the string was invalid
-	 */
+	 * @throws NumberFormatException if the string was invalid */
 	public static Color decode(String nm) {
 		return new Color(Integer.decode(nm).intValue());
 	}
-	
-	/**
-	 * Get the red byte component of this colour
+
+	/** Get the red byte component of this colour
 	 * 
-	 * @return The red component (range 0-255)
-	 */
+	 * @return The red component (range 0-255) */
 	public int red() {
 		return (int) (r * 255);
 	}
 
-	/**
-	 * Get the green byte component of this colour
+	/** Get the green byte component of this colour
 	 * 
-	 * @return The green component (range 0-255)
-	 */
+	 * @return The green component (range 0-255) */
 	public int green() {
 		return (int) (g * 255);
 	}
 
-	/**
-	 * Get the blue byte component of this colour
+	/** Get the blue byte component of this colour
 	 * 
-	 * @return The blue component (range 0-255)
-	 */
+	 * @return The blue component (range 0-255) */
 	public int blue() {
 		return (int) (b * 255);
 	}
 
-	/**
-	 * Get the alpha byte component of this colour
+	/** Get the alpha byte component of this colour
 	 * 
-	 * @return The alpha component (range 0-255)
-	 */
+	 * @return The alpha component (range 0-255) */
 	public int alpha() {
 		return (int) (a * 255);
 	}
-	
-	/** 
-	 * Packs the 4 components of this color into a 32-bit int.
+
+	/** Packs the 4 components of this color into a 32-bit int.
 	 * 
-	 * @return the packed color as a 32-bit int. 
-	 */
-	public int toIntBits () {
-		int color = ((int)(255 * a) << 24) | ((int)(255 * b) << 16) | ((int)(255 * g) << 8) | ((int)(255 * r));
+	 * @return the packed color as a 32-bit int. */
+	public int toIntBits() {
+		int color = ((int) (255 * a) << 24) | ((int) (255 * b) << 16) | ((int) (255 * g) << 8)
+				| ((int) (255 * r));
 		return color;
 	}
-	
-	/**
-	 * @see java.lang.Object#hashCode()
-	 */
+
+	/** @see java.lang.Object#hashCode() */
 	public int hashCode() {
-		return ((int) (r+g+b+a)*255);
+		return ((int) (r + g + b + a) * 255);
 	}
-	
-	/**
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
+
+	/** @see java.lang.Object#equals(java.lang.Object) */
 	public boolean equals(Object other) {
 		if (other instanceof Color) {
 			Color o = (Color) other;
@@ -193,11 +170,9 @@ public class Color {
 		}
 		return false;
 	}
-	
-	/**
-	 * @see java.lang.Object#toString()
-	 */
+
+	/** @see java.lang.Object#toString() */
 	public String toString() {
-		return "Color ("+r+","+g+","+b+","+a+")";
+		return "Color (" + r + "," + g + "," + b + "," + a + ")";
 	}
 }
